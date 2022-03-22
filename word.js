26c8d623abde5a5106f7c5c42227a54544e6efcc
andiswa-sinxo <andiswasinxo@gmail.com>
2022-03-22 15:52:46 +0200

    more functionality

--- word.js
+++ word.js
@@ -6,20 +6,49 @@ const checkbox = document.querySelector(".hide")
 const long = document.querySelector(".displayWord")
 
 
+const getLongestWord = (sentence) => {
+    let longWord = '';
+    const longWords = []
+    const words = sentence.split(' ');
+
+    words.map(word => {
+        if (word.length >= longWord.length) {
+            longWord = word
+        }
+    });
+
+    words.map(word => {
+        if (word.length == longWord.length) {
+            longWords.push(word)
+        }
+    })
+    console.log(longWords);
+    return longWords;
+}
+
+
 const wordBtnClicked = () => {
 
     const wordSentence = word.value
     const splitWord = wordSentence.split(" ");
     let wordsMoreThanFour = ""
     //let long = ""
+    const lwf = getLongestWord(wordSentence);
+
     for (let i = 0; i < splitWord.length; i++) {
         const element = splitWord[i];
         if (element.length > 4) {
             // if element === longWord
-            wordsMoreThanFour += `<mark>${element}</mark> `
-        }else {
+            if (lwf.includes(element)) {
+                wordsMoreThanFour += `<mark><u>${element}</u></mark> `
+
+            } else {
+                wordsMoreThanFour += `<mark>${element}</mark> `
+
+            }
+        } else {
             wordsMoreThanFour += `${element} `
-            
+
         }
 
     }
@@ -32,67 +61,72 @@ const wordBtnClicked = () => {
 }
 
 const hideWords = () => {
-    
+
     const wordSentence = word.value
     const split = wordSentence.split(" ");
     let notHiddenWords = ""
-    
+
     for (let i = 0; i < split.length; i++) {
         const elem = split[i];
         if ((checkbox.checked === false) && (split[i].length >= 5)) {
             notHiddenWords += `<mark>${elem}</mark> `
-        }else if((checkbox.checked === false) && (split[i].length < 5)){
+        } else if ((checkbox.checked === false) && (split[i].length < 5)) {
             notHiddenWords += `${elem} `
         }
         if ((checkbox.checked === true) && (split[i].length >= 5)) {
             notHiddenWords += `<mark>${elem}</mark> `
-        }else {
+        } else {
             notHiddenWords += ""
         }
-            display.innerHTML = notHiddenWords
+        display.innerHTML = notHiddenWords
     }
-    
+
 }
 const longestWord = () => {
     let arrWord = [0];
     const wordSentence = word.value
     const split = wordSentence.split(" ");
-    let longWord = "" 
+    let longWord = ""
     let anotherLongWord = ""
 
     for (let i = 0; i < split.length; i++) {
-       const elem = split[i];
+        const elem = split[i];
         if ((checkbox.checked === true) && (arrWord.length <= elem.length)) {
             console.log(arrWord.length < elem.length);
             arrWord = split[i]
             anotherLongWord = elem
             longWord += `<mark class= "gg">${arrWord}</mark> `
-                console.log(arrWord);
+            console.log(arrWord);
         } else {
             longWord += ""
         }
-        
+
     }
-let WordLong = []
+    let WordLong = []
     for (let i = 0; i < split.length; i++) {
         const elem = split[i];
-        if(elem.length == anotherLongWord.length){
-                WordLong.push(elem)
-                
+        if (elem.length == anotherLongWord.length) {
+            WordLong.push(elem)
+
         }
-        
-        
+
+
     }
-    long.innerHTML= longWord;
+    long.innerHTML = longWord;
     long.innerHTML = `<mark class= "gg">${WordLong}</mark> `
 }
 
 
-    
-    btn.addEventListener('click', wordBtnClicked)
-    checkbox.addEventListener("click", function(){
-        hideWords();
-        longestWord();
-    })
-    
-    
+
+btn.addEventListener('click', function () {
+    wordBtnClicked()
+    // longestWord()
+
+})
+checkbox.addEventListener("click", function () {
+    const x = getLongestWord(word.value);
+    console.log(x);
+    hideWords(),
+        longestWord()
+})
+
