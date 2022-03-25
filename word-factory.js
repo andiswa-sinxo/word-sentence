610266f8b0816e9ccfd3350ad00a4cc8a9d54610
andiswa-sinxo <andiswasinxo@gmail.com>
2022-03-25 10:46:48 +0200

    factory function

--- word-factory.js
+++ word-factory.js
@@ -1,3 +1,90 @@
-const wordSentence = () => {
-    
+const countingWords = () => {
+
+
+    const wordLength = (word) => {
+        let domLength = "";
+        if (word) {
+            const lengthWord = word.split(" ").length;
+            domLength = `Number of words in a sentence: ${lengthWord} `
+            return domLength
+        }
+    }
+
+    const WordsLongerThanFive = (sentence) => {
+
+        let high = highLight(sentence);
+
+        return high;
+    }
+
+    const highLight = (sentence) => {
+        const splitWord = sentence.split(" ");
+
+        let wordsMoreThanFour = ""
+        const lwf = getLongestWord(sentence);
+
+        for (let i = 0; i < splitWord.length; i++) {
+            const element = splitWord[i];
+            if (element.length > 4) {
+                if (lwf.includes(element)) {
+                    wordsMoreThanFour += `<mark class = "gg"><u>${element}</u></mark> `
+                } else {
+                    wordsMoreThanFour += `<mark>${element}</mark> `
+                }
+            } else {
+                wordsMoreThanFour += `${element} `
+            }
+        }
+        return wordsMoreThanFour;
+    }
+
+    const getLongestWord = (sentence) => {
+        let longWord = '';
+        const longWords = []
+
+        const words = sentence.split(' ');
+
+        words.map(word => {
+            if (word.length >= longWord.length) {
+                longWord = word
+            }
+        });
+
+        words.map(word => {
+            if (word.length == longWord.length) {
+                longWords.push(word)
+            }
+        })
+        console.log(longWords);
+        return longWords;
+    }
+
+    const checkWord = (word) => {
+        const split = word.split(" ");
+        let notHiddenWords = ""
+
+        for (let i = 0; i < split.length; i++) {
+            const elem = split[i];
+            if ((checkbox.checked === false) && (split[i].length >= 5)) {
+                notHiddenWords += `<mark>${elem}</mark> `
+            } else if ((checkbox.checked === false) && (split[i].length < 5)) {
+                notHiddenWords += `${elem} `
+            }
+            if ((checkbox.checked === true) && (split[i].length >= 5)) {
+                notHiddenWords += `<mark>${elem}</mark> `
+            } else {
+                notHiddenWords += ""
+            }
+        }
+        return notHiddenWords
+
+    }
+
+    return {
+        WordsLongerThanFive,
+        getLongestWord,
+        wordLength,
+        checkWord,
+        highLight
+    }
 }
--- word.js
+++ word.js
@@ -5,128 +5,35 @@ const length = document.querySelector(".displayLength")
 const checkbox = document.querySelector(".hide")
 const long = document.querySelector(".displayWord")
 
-
-const getLongestWord = (sentence) => {
-    let longWord = '';
-    const longWords = []
-    const words = sentence.split(' ');
-
-    words.map(word => {
-        if (word.length >= longWord.length) {
-            longWord = word
-        }
-    });
-
-    words.map(word => {
-        if (word.length == longWord.length) {
-            longWords.push(word)
-        }
-    })
-    console.log(longWords);
-    return longWords;
-}
-
+const wordWidget = countingWords()
 
 const wordBtnClicked = () => {
 
     const wordSentence = word.value
-    const splitWord = wordSentence.split(" ");
-    let wordsMoreThanFour = ""
-    //let long = ""
-    const lwf = getLongestWord(wordSentence);
+    let moreWords = wordWidget.WordsLongerThanFive(wordSentence)
+    display.innerHTML = moreWords;
+    let countWords = wordWidget.wordLength(wordSentence);
+    length.innerHTML = countWords;
 
-    for (let i = 0; i < splitWord.length; i++) {
-        const element = splitWord[i];
-        if (element.length > 4) {
-            // if element === longWord
-            if (lwf.includes(element)) {
-                wordsMoreThanFour += `<mark><u>${element}</u></mark> `
-
-            } else {
-                wordsMoreThanFour += `<mark>${element}</mark> `
-
-            }
-        } else {
-            wordsMoreThanFour += `${element} `
-
-        }
-
-    }
-    display.innerHTML = wordsMoreThanFour;
-
-    if (wordSentence) {
-        const lengthWord = wordSentence.split(" ").length;
-        length.innerHTML = `Number of words in a sentence: ${lengthWord} `
-    }
 }
 
 const hideWords = () => {
-
     const wordSentence = word.value
-    const split = wordSentence.split(" ");
-    let notHiddenWords = ""
-
-    for (let i = 0; i < split.length; i++) {
-        const elem = split[i];
-        if ((checkbox.checked === false) && (split[i].length >= 5)) {
-            notHiddenWords += `<mark>${elem}</mark> `
-        } else if ((checkbox.checked === false) && (split[i].length < 5)) {
-            notHiddenWords += `${elem} `
-        }
-        if ((checkbox.checked === true) && (split[i].length >= 5)) {
-            notHiddenWords += `<mark>${elem}</mark> `
-        } else {
-            notHiddenWords += ""
-        }
-        display.innerHTML = notHiddenWords
-    }
+    let checkWords = wordWidget.checkWord(wordSentence)
+   let checkCheck = wordWidget.highLight(checkWords);
+    display.innerHTML = checkCheck
 
 }
-const longestWord = () => {
-    let arrWord = [0];
-    const wordSentence = word.value
-    const split = wordSentence.split(" ");
-    let longWord = ""
-    let anotherLongWord = ""
-
-    for (let i = 0; i < split.length; i++) {
-        const elem = split[i];
-        if ((checkbox.checked === true) && (arrWord.length <= elem.length)) {
-            console.log(arrWord.length < elem.length);
-            arrWord = split[i]
-            anotherLongWord = elem
-            longWord += `<mark class= "gg">${arrWord}</mark> `
-            console.log(arrWord);
-        } else {
-            longWord += ""
-        }
-
-    }
-    let WordLong = []
-    for (let i = 0; i < split.length; i++) {
-        const elem = split[i];
-        if (elem.length == anotherLongWord.length) {
-            WordLong.push(elem)
-
-        }
-
-
-    }
-    long.innerHTML = longWord;
-    long.innerHTML = `<mark class= "gg">${WordLong}</mark> `
-}
-
-
 
 btn.addEventListener('click', function () {
+    const x = wordWidget.getLongestWord(word.value);
+    console.log(x);
     wordBtnClicked()
-    // longestWord()
+    
 
 })
 checkbox.addEventListener("click", function () {
-    const x = getLongestWord(word.value);
-    console.log(x);
-    hideWords(),
-        longestWord()
+    hideWords()
+   
 })
 
